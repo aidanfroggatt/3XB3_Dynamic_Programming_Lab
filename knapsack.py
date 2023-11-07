85af39e90b20d013fe5cf2bf22dad1363dd877ca
Aidan Froggatt <aidanfr@live.ca>
2023-11-07 12:10:05 -0500

    separated part 2 into its own py file

--- knapsack.py
+++ knapsack.py
@@ -140,7 +140,8 @@ def experiment_td_vs_bu_td_better():
     plt.xlabel('Number of Items')
     plt.ylabel('Runtime (seconds)')
     plt.legend()
-    plt.title('Experiment 1: TD vs. BU (TD Better)')
+    plt.title('Experiment 2: TD vs. BU (BU Better)')
+    # plt.title('Experiment 1: TD vs. BU (TD Better)')
     plt.show()
 
 
@@ -168,42 +169,11 @@ def experiment_td_vs_bu_bu_better():
     plt.xlabel('Number of Items')
     plt.ylabel('Runtime (seconds)')
     plt.legend()
-    plt.title('Experiment 2: TD vs. BU (BU Better)')
+    # plt.title('Experiment 2: TD vs. BU (BU Better)')
+    plt.title('Experiment 1: TD vs. BU (TD Better)')
     plt.show()
 
 
 # experiment_rec_vs_brute_force()
 # experiment_td_vs_bu_td_better()
 # experiment_td_vs_bu_bu_better()
-
-# PART 2
-def num_of_wc_runs(n, m):
-    memo = {}
-
-    def dp(n, m):
-        if (n, m) in memo:
-            return memo[(n, m)]
-
-        if m == 0:
-            return 0
-        if n == 1:
-            return m - 1
-
-        min_runs = float('inf')
-        for k in range(1, n + 1):
-            runs = 1 + max(dp(k - 1, m - 1), dp(n - k, m))
-            min_runs = min(min_runs, runs)
-
-        memo[(n, m)] = min_runs
-        return min_runs
-
-    return dp(n, m)
-
-
-def next_setting(n, m):
-    if m == 1:
-        return 1  # Linear search with one brick
-
-    # To minimize the worst-case runs, choose k that splits the remaining settings roughly in half.
-    return (n + 1) // 2
-
---
+++ part2.py
@@ -0,0 +1,32 @@
+
+# PART 2
+def num_of_wc_runs(n, m):
+    memo = {}
+
+    def dp(n, m):
+        if (n, m) in memo:
+            return memo[(n, m)]
+
+        if m == 0:
+            return 0
+        if n == 1:
+            return m - 1
+
+        min_runs = float('inf')
+        for k in range(1, n + 1):
+            runs = 1 + max(dp(k - 1, m - 1), dp(n - k, m))
+            min_runs = min(min_runs, runs)
+
+        memo[(n, m)] = min_runs
+        return min_runs
+
+    return dp(n, m)
+
+
+def next_setting(n, m):
+    if m == 1:
+        return 1  # Linear search with one brick
+
+    # To minimize the worst-case runs, choose k that splits the remaining settings roughly in half.
+    return (n + 1) // 2
+
